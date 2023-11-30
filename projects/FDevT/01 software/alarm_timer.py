c4e7c6921e1e77a97e42db4aa20625e4f325bf62
Joe G <grabow@amesys.de>
2023-11-30 18:49:02 +0100

    Version 1.0

---
+++ projects/FDevT/01 software/alarm_timer.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 29 08:24:08 2023
+
+@author: Grabow
+"""
+__version__ = '1.0'
+__author__ = 'Joe Grabow'
+
+from machine import Timer
+import ssd1306py as lcd
+
+class CountdownTimer:
+    """A class for a countdown timer. The interrupt time is exactly one second"""
+    ALARM_OFF = 1
+    ALARM_ON = 0
+    
+    def __init__(self):
+        """Initialize the CountdownTimer."""
+        self.timer = Timer(-1)  # next Timer
+        self.alarm_set = self.ALARM_OFF
+        self.sec_before = 5
+
+    def set_timer(self, minutes, seconds):
+        """Set the timer to the specified minutes and seconds."""
+        self.total_seconds = minutes * 60 + seconds
+        self.alarm_set = self.ALARM_OFF
+
+    def get_remaining_time(self):
+        """Remaining the CountdownTimer."""
+        remaining_minutes, remaining_seconds = divmod(self.total_seconds, 60)
+        return remaining_minutes, remaining_seconds, self.total_seconds
+
+    def get_alarm(self):
+        """Get Alarm Flag."""
+        return self.alarm_set
+
+    def display_time(self):
+        """Displays the time on the OLED."""
+        r_min, r_sec = divmod(self.total_seconds, 60)
+        oled_time = f"{r_min:02d}:{r_sec:02d}"
+        lcd.text(oled_time, 0, 16, 32)
+        lcd.show()
+        print(oled_time)
+
+    def timer_callback(self, timer):
+        """Callback for Timer-Interrupt."""
+        self.total_seconds -= 1
+        self.display_time()
+
+        if self.total_seconds <= 0:
+            self.timer.deinit()  # stop Timer stoppen
+            self.alarm_set = self.ALARM_ON  # time has expired
+
+        # Beep x seconds before timer ends
+        elif self.total_seconds == self.sec_before:
+            print("Beep")
+
+    def start_timer(self):  # only start without changing the time
+        """Start Timer."""
+        print('Timer Start')
+        self.timer.init(period=1000, mode=Timer.PERIODIC, callback=self.timer_callback)
+
+    def pause_timer(self):
+        """Wait Timer."""        
+        self.timer.deinit()
+        self.alarm_set = 1  # alarm off
+
+    def set_beep(self, seconds_before):
+        """Beep befor the timer is finished."""      
+        self.sec_before = seconds_before
+
+# ESP32 Pin assignment for OLED and I2C
+oled_width = 128 
+oled_height = 64
+scl = 22
+sda = 21
+
+lcd.init_i2c(scl, sda, oled_width, oled_height)
+lcd.contrast(100)  # OLED-Contrast, max contrast is 255
+
+
--- projects/FDevT/01 software/main.py
+++ projects/FDevT/01 software/main.py
@@ -8,9 +8,11 @@ __version__ = '1.0'
 __author__ = 'Joe Grabow'
 
 import ssd1306py as lcd
+import ujson
 from machine import Pin, Timer, SoftI2C
 from button_handle import ButtonHandler
 from state_machine import StateMachine
+from alarm_timer import CountdownTimer
 
 # Debugging
 debug = True
@@ -24,10 +26,32 @@ oled_height = 64
 scl = 22
 sda = 21
 
-# use Functions and Classes
-button_handler = ButtonHandler(button_pins)
-sm = StateMachine()
+# all variables
+# Variable set for the recipe
+D_Min = 0
+D_Sec = 0
+S_Min = 0
+S_Sec = 0
+F_Min = 0
+F_Sec = 0
 
+# Oled-Menue Variable 
+DEV_oled_time, STOP_oled_time, FIX_oled_time = '00:00', '00:00', '00:00'
+
+
+# Initialize button handler, state machine, and countdown timer
+#button_handler = ButtonHandler(button_pins)
+#sm = StateMachine()
+#count_timer = CountdownTimer()
+
+# Function for initialization
+def initialize():
+    global button_handler, sm, count_timer
+
+    # Initialize button handler, state machine, and countdown timer
+    button_handler = ButtonHandler(button_pins)
+    sm = StateMachine()
+    count_timer = CountdownTimer()
 
 # User State-Functions
 def s_f_1():
@@ -42,138 +66,233 @@ def s_f_1():
     lcd.text('  RECIPES', 0, 40, 16)
     lcd.show()
 
+# PROCESS DEV init
 def s_f_2():
-    # PROCESS DEV
-    # load Recipes
-    # Timer = DEV
+    global D_Min, D_Sec, S_Min, S_Sec, F_Min, F_Sec
+    global DEV_oled_time, STOP_oled_time, FIX_oled_time
+    
     if debug:
         print('State 2')
+    
+    # load recipe
+    with open('recipes.json', 'r') as file:
+        data = ujson.load(file)
+        
+    D_Min, D_Sec = data["D_Min"], data["D_Sec"]
+    S_Min, S_Sec = data["S_Min"], data["S_Sec"]
+    F_Min, F_Sec = data["F_Min"], data["F_Sec"]
+
+    # Recipe times
+    DEV_oled_time = f"{D_Min:02d}:{D_Sec:02d}"
+    STOP_oled_time = f"{S_Min:02d}:{S_Sec:02d}"
+    FIX_oled_time = f"{F_Min:02d}:{F_Sec:02d}"
+    
+    count_timer.set_timer(D_Min, D_Sec)  # initialise Timer
+
+    # OLED Display
     lcd.clear()
     lcd.text('DEVELOPER INIT', 0, 0, 16)
-    lcd.text('08:00', 0, 16, 32)
+    lcd.text(DEV_oled_time, 0, 16, 32)
     #lcd.text('>', 115, 16, 32)  # skip
     lcd.text('<', 85,16,32)  # return
     lcd.text('#', 99,16,32)  # wait
     #lcd.text('*', 99,16,32)  # run
-    lcd.text('08:00', 0, 48, 16)
+    lcd.text(DEV_oled_time, 0, 48, 16)
     lcd.show()
 
+
+# PROCESS DEV-Timer run
 def s_f_3():
-    # DEV-Timer run
+    global DEV_oled_time
+    
     if debug:
         print('State 3')
+    
+    [r_min, r_sec, t_sec] = count_timer.get_remaining_time()
+    oled_time = f"{r_min:02d}:{r_sec:02d}"
+    
+    # OLED Menue
     lcd.clear()
     lcd.text('DEVELOPER RUN', 0, 0, 16)
-    lcd.text('00:00', 0, 16, 32)
+    lcd.text(oled_time, 0, 16, 32)
     lcd.text('>', 115, 16, 32)  # skip
     lcd.text('<', 85,16,32)  # return
     #lcd.text('#', 99,16,32)  # wait
     lcd.text('*', 99,16,32)  # run
-    lcd.text('08:00', 0, 48, 16)
-    lcd.show()        
+    lcd.text(DEV_oled_time, 0, 48, 16)
+    lcd.show()
+    
+    count_timer.start_timer()  # DEV-Timer run    
 
+
+# PROCESS DEV-Timer wait
 def s_f_4():
-    # DEV-Timer wait
+    global DEV_oled_time
+    
     if debug:
         print('State 4')
+        
+    count_timer.pause_timer()
+    [r_min, r_sec, t_sec] = count_timer.get_remaining_time()  # determine remaining time
+    oled_time = f"{r_min:02d}:{r_sec:02d}"
+    
+    # OLED Menue
     lcd.clear()
-    lcd.text('DEVELOPER STOP', 0, 0, 16)
-    lcd.text('00:00', 0, 16, 32)
+    lcd.text('DEVELOPER WAIT', 0, 0, 16)
+    lcd.text(oled_time, 0, 16, 32)
     #lcd.text('>', 115, 16, 32)  # skip
     lcd.text('<', 85,16,32)  # return
     lcd.text('#', 99,16,32)  # wait
     #lcd.text('*', 99,16,32)  # run
-    lcd.text('08:00', 0, 48, 16)
+    lcd.text(DEV_oled_time, 0, 48, 16)
     lcd.show()     
 
+
+# PROCESS STOP init
 def s_f_5():
-    # Timer = STOP
+    global STOP_oled_time
+    
     if debug:
         print('State 5')
+        
+    count_timer.set_timer(S_Min, S_Sec)  # initialise STOP-Timer
+    oled_time = f"{S_Min:02d}:{S_Sec:02d}"
+    
+    # Oled Menue
     lcd.clear()
     lcd.text('STOP INIT', 0, 0, 16)
-    lcd.text('00:30', 0, 16, 32)
+    lcd.text(oled_time, 0, 16, 32)
     #lcd.text('>', 115, 16, 32)  # skip
     lcd.text('<', 85,16,32)  # return
     lcd.text('#', 99,16,32)  # wait
     #lcd.text('*', 99,16,32)  # run
-    lcd.text('00:30', 0, 48, 16)
+    lcd.text(STOP_oled_time, 0, 48, 16)
     lcd.show()  
 
+
+# PROCESS STOP-Timer run
 def s_f_6():
-    # STOP-Timer run
+    global STOP_oled_time
+        
     if debug:
         print('State 6')
+        
+    [r_min, r_sec, t_sec] = count_timer.get_remaining_time()  # determine remaining time
+    oled_time = f"{r_min:02d}:{r_sec:02d}"
+    
+    # Oled Menue
     lcd.clear()
     lcd.text('STOP RUN', 0, 0, 16)
-    lcd.text('00:30', 0, 16, 32)
+    lcd.text(oled_time, 0, 16, 32)
     lcd.text('>', 115, 16, 32)  # skip
     lcd.text('<', 85,16,32)  # return
     #lcd.text('#', 99,16,32)  # wait
     lcd.text('*', 99,16,32)  # run
-    lcd.text('00:30', 0, 48, 16)
-    lcd.show()  
+    lcd.text(STOP_oled_time, 0, 48, 16)
+    lcd.show()
 
+    count_timer.start_timer()  # STOP-Timer run   
+
+
+# PROCESS STOP-Timer wait
 def s_f_7():
-    # STOP-Timer wait
+    global STOP_oled_time
+        
     if debug:
         print('State 7')
+        
+    count_timer.pause_timer()
+    [r_min, r_sec, t_sec] = count_timer.get_remaining_time()  # determine remaining time
+    oled_time = f"{r_min:02d}:{r_sec:02d}"
+
+    # Oled Menue
     lcd.clear()
     lcd.text('STOP WAIT', 0, 0, 16)
-    lcd.text('00:30', 0, 16, 32)
+    lcd.text(oled_time, 0, 16, 32)
     #lcd.text('>', 115, 16, 32)  # skip
     lcd.text('<', 85,16,32)  # return
     lcd.text('#', 99,16,32)  # wait
     #lcd.text('*', 99,16,32)  # run
-    lcd.text('00:30', 0, 48, 16)
+    lcd.text(STOP_oled_time, 0, 48, 16)
     lcd.show()  
 
+
+# Process FIX init
 def s_f_8():
-    # TIMER = FIX
+    global FIX_oled_time
+        
     if debug:
         print('State 8')
+
+    count_timer.set_timer(F_Min, F_Sec)  # initialise FIX-Timer
+    oled_time = f"{F_Min:02d}:{F_Sec:02d}"
+
+    # Oled Menue
     lcd.clear()
     lcd.text('FIX INIT', 0, 0, 16)
-    lcd.text('04:30', 0, 16, 32)
+    lcd.text(oled_time, 0, 16, 32)
     #lcd.text('>', 115, 16, 32)  # skip
     lcd.text('<', 85,16,32)  # return
     lcd.text('#', 99,16,32)  # wait
     #lcd.text('*', 99,16,32)  # run
-    lcd.text('00:30', 0, 48, 16)
+    lcd.text(FIX_oled_time, 0, 48, 16)
     lcd.show()  
 
+
+# Process FIX run
 def s_f_9():
-    # FIX-Timer run
+    global FIX_oled_time
+        
     if debug:
         print('State 9')
+        
+    [r_min, r_sec, t_sec] = count_timer.get_remaining_time()  # determine remaining time
+    oled_time = f"{r_min:02d}:{r_sec:02d}"        
+
+    # Oled Menue
     lcd.clear()
     lcd.text('FIX RUN', 0, 0, 16)
-    lcd.text('04:30', 0, 16, 32)
+    lcd.text(oled_time, 0, 16, 32)
     lcd.text('>', 115, 16, 32)  # skip
     lcd.text('<', 85,16,32)  # return
     #lcd.text('#', 99,16,32)  # wait
     lcd.text('*', 99,16,32)  # run
-    lcd.text('00:30', 0, 48, 16)
-    lcd.show()  
+    lcd.text(FIX_oled_time, 0, 48, 16)
+    lcd.show()
+    
+    count_timer.start_timer()  # FIX-Timer run   
 
+
+# PROCESS FIX-Timer wait
 def s_f_10():
-    # FIX-Timer wait
+    global FIX_oled_time
+        
     if debug:
         print('State 10')
+
+    count_timer.pause_timer()
+    [r_min, r_sec, t_sec] = count_timer.get_remaining_time()  # determine remaining time
+    oled_time = f"{r_min:02d}:{r_sec:02d}"
+
+    # Oled Menue
     lcd.clear()
     lcd.text('FIX WAIT', 0, 0, 16)
-    lcd.text('04:30', 0, 16, 32)
+    lcd.text(oled_time, 0, 16, 32)
     #lcd.text('>', 115, 16, 32)  # skip
     lcd.text('<', 85,16,32)  # return
     lcd.text('#', 99,16,32)  # wait
     #lcd.text('*', 99,16,32)  # run
-    lcd.text('00:30', 0, 48, 16)
+    lcd.text(FIX_oled_time, 0, 48, 16)
     lcd.show()  
 
 def s_f_11():
     # RECIPES end
     if debug:
         print('State 11')
+    
+    count_timer.pause_timer()
+    
+    # Oled Menue    
     lcd.clear()
     lcd.text('PROCESS', 0, 0, 16)
     lcd.text('END', 0, 16, 32)
@@ -200,21 +319,23 @@ statefunctions = {
 
 
 # mainloop
+initialize()
+
 lcd.init_i2c(scl, sda, oled_width, oled_height)
 lcd.contrast(100)  # OLED-Contrast, max contrast is 255
 
-state = 1  # Start State
-s_f_1()
+state = 1  # Start State (PROCESS)
+s_f_1()  # User-State PROCESS
 
 while True:
-    if button_handler.button_click != 0:  # Button Click
-        state = sm.transition(state, 11, int(button_handler.button_click), 1)  # Trigger State-machine  
+    if (button_handler.button_click != 0) or (count_timer.get_alarm() == 0):  # Button Click or timer == 0
+        state = sm.transition(state, 11, int(button_handler.button_click), count_timer.get_alarm())  # Trigger State-machine  
         
         if state in statefunctions:  # Call User-State-Function
             statefunctions[state]()
         else:
             print("state not defind")
 
-#        print(str(button_handler.button_click))
         button_handler.button_click = 0  # Reset Button
+
 
